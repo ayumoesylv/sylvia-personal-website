27e1090dd0e3a510b7520d791a8c9660afe1e0e9
sylviazhang <sylvia.zhang88@outlook.com>
2025-09-29 13:41:21 -0400

    first commit

---
+++ src/AboutMe/Page.jsx
@@ -0,0 +1,75 @@
+import {useState} from "react";
+import selfie from "/Users/sylviazhang/vscode projects/Personal Website/personal-website/src/IMG_1313.jpeg";
+import bioData from "./bioData"
+
+function Heading() {
+    const [showBio, setShowBio] = useState(false)
+
+    function handleShowBio() {
+        setShowBio(showBio => !showBio)
+    }
+
+    function determineText() {
+        if (showBio) {
+            return "Click me to hide my bio!"
+        } else {
+            return "Click me to show my bio!"
+        }
+    }
+
+    return (
+        <div className = "heading-container">
+            <div className = "vertical-container">
+                <img src = {selfie} className="headshot" width={250} height="auto" alt = "Sylvia" onClick={handleShowBio}/>
+                <p>{determineText()}</p>
+            </div>
+            <div className = "vertical-container">
+                <h1 className="big-text">Sylvia Zhang</h1>
+                {showBio ? <Bio /> : null}
+            </div>
+        </div>
+    )
+}
+
+function MainContent() {
+    return (
+        <div>
+            <p>
+                Hi! I’m Sylvia. It’s nice to meet you! I’m a first year at Cornell interested in software engineering, artificial intelligence, and endangered languages. Some of my hobbies include Model UN, digital art, and watching horror game let’s plays! Something I’d love to learn how to do is to bake!
+            </p>
+        </div>
+    )
+}
+
+function Bio() {
+    const bioComponents = bioData.map((bio) => {
+        return <BioElement img = {bio.img} title = {bio.title} text = {bio.text}/>
+    })
+    return (
+        <div>
+            {bioComponents}
+        </div>
+    )
+}
+
+function BioElement(props) {
+    return (
+        <>
+            <img src = {props.img}/>
+            <h2>{props.title}</h2>
+            <p>{props.text}</p>
+        </>
+    )
+}
+
+export default function AboutMe() {
+    
+
+    return (
+        <div>
+            <Heading />
+            
+            <MainContent />
+        </div>
+    )
+}
---
+++ src/AboutMe/bioData.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        img: "/Users/sylviazhang/vscode projects/Personal Website/personal-website/src/star cluster.png",
+        title: "Birthday",
+        text: "August 31 2007"
+    },
+    {
+        img: "/Users/sylviazhang/vscode projects/Personal Website/personal-website/src/star cluster.png",
+        title: "Zodiac",
+        text: "Virgo"
+    },
+    {
+        img: "/Users/sylviazhang/vscode projects/Personal Website/personal-website/src/star cluster.png",
+        title: "College",
+        text: "Cornell University"
+    },
+    
+]
