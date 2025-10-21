b43eeb63fc084b21c4c5f2a511676259b74845b2
sylviazhang <sylvia.zhang88@outlook.com>
2025-10-21 14:50:36 -0400

    contact me

--- src/ContactMe/Page.jsx
+++ src/ContactMe/Page.jsx
@@ -1,5 +1,30 @@
 export default function Contact() {
+    function handleSubmit(formData) {
+        const firstName = formData.get("firstName")
+    }
     return (
-        <h2>Contact me page!</h2>
+        <div>
+            <h2>Contact me page!</h2>
+            <form className = "contact-form" action = {handleSubmit}>
+                <label htmlFor = "firstName">First Name: </label>
+                <input id = "firstName" />
+
+                <label htmlFor = "lastName">Last Name: </label>
+                <input id = "lastName" />
+
+                <label htmlFor = "email">Email: </label>
+                <input id = "email" type = "email"/>
+                
+                <label htmlFor = "comment">Comment: </label>
+                <textarea id = "comment"></textarea>
+
+                <button>Submit!</button>
+            </form>
+            <div>
+
+            </div>
+        </div>
+        
+
     )
 }
