157a752d464dcf2324c0028b5530cfcda439fe32
sylviazhang <sylvia.zhang88@outlook.com>
2025-10-25 09:11:05 -0400

    navbar

--- src/MyProjects/Page.jsx
+++ src/MyProjects/Page.jsx
@@ -1,10 +1,11 @@
 import techData from "./techData"
+import Contacts from "./../ConstantComponents/Contacts"
 
 function TechElement({tech, years}) {
     return (
         <div>
             <p>{tech}</p>
-            <p>{years}</p>
+            <p>{years} years</p>
         </div>
     )
 }
@@ -17,22 +18,27 @@ export default function MyProjects() {
     })
     return (
         <div>
+            <div className = "projects-container">
             <h1>PROJECTS</h1>
-            <h2>About my personal website development</h2>
-            <p>I was inspired to make a personal website after I went to Big Red Hacks 
-                and started learning React! With absolutely no experience at all, 
-                I tried (and struggled) with vibecoding, working in a team, 
-                managing a project in GitHub, and following tutorials. 
-                Importantly though, it was fun. When I saw the projects that the winning teams made, 
-                I remember feeling so blown away with what was possible. 
-                I really wanted to develop those skills for myself, 
-                and so I decided to create this website! 
-                I spent some time self learning and got to coding. 
-                It’s been difficult doing this all from scratch 
-                and navigating through the error messages, 
-                but I’ve definitely grown a lot! </p>
-            <h2>Skills and technologies</h2>
-            <div>{techComponents}</div>
+            <div className = "personal-website">
+                <h2>Personal website</h2>
+                <p>I was inspired to make a personal website after I went to Big Red Hacks 
+                    and started learning React! With absolutely no experience at all, 
+                    I tried (and struggled) with vibecoding, working in a team, 
+                    managing a project in GitHub, and following tutorials. 
+                    Importantly though, it was fun. When I saw the projects that the winning teams made, 
+                    I remember feeling so blown away with what was possible. 
+                    I really wanted to develop those skills for myself, 
+                    and so I decided to create this website! 
+                    I spent some time self learning and got to coding. 
+                    It’s been difficult doing this all from scratch 
+                    and navigating through the error messages, 
+                    but I’ve definitely grown a lot! </p>
+                <h2>Skills</h2>
+                <div>{techComponents}</div>
+            </div>
+            </div>
+            <Contacts />
         </div>
         
 
